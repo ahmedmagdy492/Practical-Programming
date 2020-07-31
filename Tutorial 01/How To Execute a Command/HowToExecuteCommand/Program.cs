dfc24414b8dbf3ec21ff2c9090a09258df0fa373
ahmed magdy <a_midw@yahoo.com>
2020-07-31 20:26:39 +0200

    Tutorial 01

---
+++ Tutorial 01/How To Execute a Command/HowToExecuteCommand/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace HowToExecuteCommand
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Enter Your Name: ");
+            string name = ReadLine();
+            string result = CommandExecutor.ExecuteCommand($"echo {name}");
+            WriteLine(result);
+        }
+    }
+}
