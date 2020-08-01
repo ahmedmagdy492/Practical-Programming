76aa8ed9601c73d3060b90912632f30d6e6fdec0
ahmed magdy <a_midw@yahoo.com>
2020-08-01 04:42:43 +0200

    process list

---
+++ Tutorial 01/Processes List/ProcessList/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace ProcessList
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnKill = new System.Windows.Forms.Button();
+            this.lsProcess = new System.Windows.Forms.ListBox();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.btnKill);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1145, 71);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // btnKill
+            // 
+            this.btnKill.Location = new System.Drawing.Point(3, 3);
+            this.btnKill.Name = "btnKill";
+            this.btnKill.Size = new System.Drawing.Size(121, 48);
+            this.btnKill.TabIndex = 0;
+            this.btnKill.Text = "Kill";
+            this.btnKill.UseVisualStyleBackColor = true;
+            this.btnKill.Click += new System.EventHandler(this.btnKill_Click);
+            // 
+            // lsProcess
+            // 
+            this.lsProcess.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lsProcess.FormattingEnabled = true;
+            this.lsProcess.ItemHeight = 25;
+            this.lsProcess.Location = new System.Drawing.Point(0, 71);
+            this.lsProcess.Name = "lsProcess";
+            this.lsProcess.Size = new System.Drawing.Size(1145, 532);
+            this.lsProcess.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1145, 603);
+            this.Controls.Add(this.lsProcess);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Main";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button btnKill;
+        private System.Windows.Forms.ListBox lsProcess;
+    }
+}
+
---
+++ Tutorial 01/Processes List/ProcessList/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace ProcessList
+{
+    public partial class Form1 : Form
+    {
+        private readonly Process[] processes;
+        public Form1()
+        {
+            InitializeComponent();
+            processes = Process.GetProcesses();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach(var item in processes)
+            {
+                lsProcess.Items.Add(item.ProcessName);
+            }
+        }
+
+        private void btnKill_Click(object sender, EventArgs e)
+        {
+            if(lsProcess.SelectedItems.Count == 1)
+            {
+                string processName = (string)lsProcess.SelectedItem;
+                var process = processes.FirstOrDefault(p => p.ProcessName == processName);
+                process.Kill();
+            }
+        }
+    }
+}
