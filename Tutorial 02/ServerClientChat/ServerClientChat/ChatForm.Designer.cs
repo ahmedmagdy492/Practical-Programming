e519db07d068a989e253535c82ec58a93c2cdc01
ahmed magdy <a_midw@yahoo.com>
2020-08-20 14:27:26 +0200

    'addedclient'

---
+++ Tutorial 02/ServerClientChat/ServerClientChat/ChatForm.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace ServerClientChat
+{
+    partial class ChatForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.txtMsg = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.lsMsgs = new System.Windows.Forms.ListBox();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.txtMsg);
+            this.flowLayoutPanel1.Controls.Add(this.btnSend);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 402);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Padding = new System.Windows.Forms.Padding(15);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(954, 107);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // txtMsg
+            // 
+            this.txtMsg.Location = new System.Drawing.Point(18, 18);
+            this.txtMsg.Multiline = true;
+            this.txtMsg.Name = "txtMsg";
+            this.txtMsg.Size = new System.Drawing.Size(770, 77);
+            this.txtMsg.TabIndex = 0;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(794, 18);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(138, 77);
+            this.btnSend.TabIndex = 1;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            // 
+            // lsMsgs
+            // 
+            this.lsMsgs.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lsMsgs.FormattingEnabled = true;
+            this.lsMsgs.ItemHeight = 25;
+            this.lsMsgs.Location = new System.Drawing.Point(0, 0);
+            this.lsMsgs.Name = "lsMsgs";
+            this.lsMsgs.Size = new System.Drawing.Size(954, 402);
+            this.lsMsgs.TabIndex = 1;
+            // 
+            // ChatForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(954, 509);
+            this.Controls.Add(this.lsMsgs);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "ChatForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Chat";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.TextBox txtMsg;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.ListBox lsMsgs;
+    }
+}
--- Tutorial 02/ServerClientChat/ServerClientChat/Form1.cs
+++ Tutorial 02/ServerClientChat/ServerClientChat/Form1.cs
@@ -15,6 +15,7 @@ namespace ServerClientChat
     public partial class Form1 : Form
     {
         private readonly Socket socket;
+        private Socket clientSocket;
         private const int PORT = 9999;
         public Form1()
         {
@@ -22,13 +23,27 @@ namespace ServerClientChat
             socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
         }
 
-        private void btnStart_Click(object sender, EventArgs e)
+        private async void btnStart_Click(object sender, EventArgs e)
         {
             socket.Bind(new IPEndPoint(new IPAddress(new byte[] { 127, 0, 0, 1 }), PORT));
             socket.Listen(5);
             btnStart.Enabled = false;
             btnStart.Text = "Listining on port " + PORT + ".....";
 
+            clientSocket = await socket.AcceptAsync();
+
+            ChatForm chat = new ChatForm();
+            chat.Text = socket.LocalEndPoint.ToString();
+            chat.ShowDialog();
+        }
+
+        private async void btnConnect_Click(object sender, EventArgs e)
+        {
+            await socket.ConnectAsync(new IPEndPoint(new IPAddress(new byte[] { 127, 0, 0, 1}), PORT));
+
+            ChatForm chat = new ChatForm();
+            chat.Text = socket.LocalEndPoint.ToString();
+            chat.ShowDialog();
         }
     }
 }
